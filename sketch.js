a7348638c44e7bde6222551fc641845c7db186c6
rachelebarazzetta <rachele.baraz@gmail.com>
2018-10-07 11:44:00 +0200

    1st change

--- sketch.js
+++ sketch.js
@@ -1,11 +1,15 @@
 function preload(){
   // put preload code here
+
 }
 
 function setup() {
   // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  background(115);
 }
 
 function draw() {
   // put drawing code here
+  line(89, 40, 50, 20);
 }
